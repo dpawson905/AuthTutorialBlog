2ba78fd5765578067b7cc0abbcb7019aa3339f53
Darrell Pawson <dpawson905@gmail.com>
2019-07-12 13:55:11 -0400

    add local bootstrap and jquery files to fix error in console

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,9 +5,11 @@ function checkPasswordMatch() {
   var confirmPassword = $('#password2').val();
 
   if (password != confirmPassword)
-    $('#checkPw').addClass('form-error').removeClass('form-success').html('Passwords do not match!');
+    $('#checkPw').addClass('form-error').removeClass('form-success').html('Passwords do not match!'),
+    $('#registerButton').addClass('disabled mt-4');
   else
-    $('#checkPw').addClass('form-success').removeClass('form-error').html('Passwords match.');
+    $('#checkPw').addClass('form-success').removeClass('form-error').html('Passwords match.'),
+    $('#registerButton').removeClass('disabled');
 }
 
 $(function() {
