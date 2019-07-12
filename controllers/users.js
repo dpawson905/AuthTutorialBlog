c22cef998c31c2c4a22b86ccb044474d88d6d264
Darrell Pawson <dpawson905@gmail.com>
2019-07-12 14:00:01 -0400

    set up initial post routes for signup and login

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,15 @@ module.exports = {
     res.render('auth/signup');
   },
 
+  async postSignup(req, res, next) {
+    res.send('Placeholder until code is implemented');
+  },
+
   getLogin(req, res, next) {
     res.render('auth/login');
+  },
+
+  async postLogin(req, res, next) {
+    res.send('Placeholder until code is implemented');
   }
 }
--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,21 @@ const router = express.Router();
 
 const {
   getSignup,
-  getLogin
+  getLogin,
+  postSignup,
+  postLogin
 } = require('../controllers/users');
 
 /* GET signup. */
 router.get('/signup', getSignup);
 
+/* POST signup */
+router.post('/signup', postSignup);
+
 /* GET login. */
 router.get('/login', getLogin);
+
+/* POST signup */
+router.post('/login', postLogin);
 
 module.exports = router;
