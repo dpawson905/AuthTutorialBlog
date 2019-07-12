0d1f0c54996e6a121a5369c4dbfb2293538c4335
Darrell Pawson <dpawson905@gmail.com>
2019-07-11 21:34:06 -0400

    added more to boilerplate to make it easier to being the tutorial

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
+require('dotenv').config();
+
+const debug = require('debug')('siteauth:main');
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
+/* Set up the connection to the Database */
+mongoose.connect(process.env.DB_URL, {
+  useCreateIndex: true,
+  useNewUrlParser: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+  debug('Connected to MongoDB');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,9 +35,36 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+app.use(helmet());
+
+/* Set a varialble called sess and set some up some cookie stuff */
+var sess = {
+  secret: process.env.COOKIE_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { httpOnly: true, expires: Date.now() + 1000 * 60 * 60 * 24 * 7, maxAge: 1000 * 60 * 60 * 24 * 7}
+}
+
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1) // trust first proxy
+  sess.cookie.secure = true // serve secure cookies
+}
+
+app.use(flash());
+app.use(session(sess));
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  res.locals.title = 'Auth Tuts';
+  res.locals.currentUser = req.user;
+  res.locals.isAuthenticated = req.user ? true : false;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -28,14 +75,10 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function (err, req, res, next) {
+  console.log(err.stack);
+  req.flash('error', err.message);
+  res.redirect('/');
 });
 
 module.exports = app;
